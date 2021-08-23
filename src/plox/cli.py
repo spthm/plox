b9f018973befcf47013c2e7c2de4c807572978b4
Sam Thomson <2648269+spthm@users.noreply.github.com>
2021-08-23 22:13:38 +0100

    Add basic CLI argument parsing

--- src/plox/cli.py
+++ src/plox/cli.py
@@ -1,2 +1,10 @@
+import argparse
+
+
 def main():
-    print("Hello, World!")
+    parser = argparse.ArgumentParser(
+        "plox", formatter_class=argparse.ArgumentDefaultsHelpFormatter
+    )
+    parser.add_argument("script", metavar="FILE", nargs="?", help="Lox script to run")
+
+    args = parser.parse_args()
