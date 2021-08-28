5acc7f8453e2daf6b88f09c7af06a9f7be7ab1c0
Sam Thomson <2648269+spthm@users.noreply.github.com>
2021-08-28 18:23:39 +0100

    Add equality, repr for Tokens to aid testing/debugging.

--- src/plox/tokens.py
+++ src/plox/tokens.py
@@ -1,3 +1,5 @@
+from __future__ import annotations
+
 from enum import Enum, auto, unique
 
 
@@ -58,6 +60,26 @@ class Token:
         self._lexeme = lexeme
         self._literal = literal
         self._lno = lno
+
+    def __eq__(self, other: Token):
+        return (
+            self._kind == other._kind
+            and self._lexeme == other._lexeme
+            and self._literal == other._literal
+            and self._lno == other._lno
+        )
+
+    def __repr__(self):
+        # We use repr() for lexeme and literal so that, respectively, the empty string
+        # and None are printed.
+        return (
+            "Token("
+            f"kind={self._kind}"
+            f", lexeme={repr(self._lexeme)}"
+            f", literal={repr(self._literal)}"
+            f", lno={self._lno}"
+            ")"
+        )
 
     def __str__(self):
         return f"{self._kind} {self._lexeme} {self._literal}"
