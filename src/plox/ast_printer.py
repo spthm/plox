8665102f88998a38522e3e95df2f2ecc23df7398
Sam Thomson <2648269+spthm@users.noreply.github.com>
2021-08-30 15:54:19 +0100

    [5.4] Add ast printing.
    We don't need to implement the full Visitor Pattern in Python, we can
    get away with the (IMO) cleaner single dispatch. I believe this gives
    the same level of static type checking we'd get with the Visitor
    Pattern, but is more readable - module some hacks to workaround mypy's
    lack of support for @overload on an @singledispatch function.

---
+++ src/plox/ast_printer.py
@@ -0,0 +1,48 @@
+from functools import singledispatch
+from typing import overload
+
+from plox.expressions import Binary, Expr, Grouping, Literal, Unary
+
+
+def _parenthesize(name: str, *args: Expr) -> str:
+    content = " ".join(ast_str(e) for e in args)
+    return f"({name} {content})"
+
+
+# mypy's @overload is buggy for @singledispatch. Use of the separate _ast_str here
+# is a workaround https://github.com/python/mypy/issues/8356.
+
+
+@singledispatch
+def _ast_str(expr: Expr) -> str:
+    raise TypeError(f"ast_str does not support {type(expr)}")
+
+
+@overload
+@_ast_str.register(Binary)
+def ast_str(expr: Binary) -> str:
+    return _parenthesize(expr.operator._lexeme, expr.left, expr.right)
+
+
+@overload
+@_ast_str.register(Grouping)
+def ast_str(expr: Grouping) -> str:
+    return _parenthesize("group", expr.expression)
+
+
+@overload
+@_ast_str.register(Literal)
+def ast_str(expr: Literal) -> str:
+    if expr.value is None:
+        return "nil"
+    return str(expr.value)
+
+
+@overload
+@_ast_str.register(Unary)
+def ast_str(expr: Unary) -> str:
+    return _parenthesize(expr.operator._lexeme, expr.right)
+
+
+def ast_str(expr: Expr) -> str:
+    return _ast_str(expr)
---
+++ tests/test_ast_printer.py
@@ -0,0 +1,21 @@
+from plox.ast_printer import ast_str
+from plox.expressions import Binary, Grouping, Literal, Unary
+from plox.tokens import Token, TokenType
+
+
+def test_literal_int():
+    assert ast_str(Literal(123)) == "123"
+
+
+def test_literal_float():
+    assert ast_str(Literal(45.67)) == "45.67"
+
+
+def test_binry_unary_grouping_literal():
+    minus = Token(TokenType.MINUS, "-", None, 1)
+    lhs = Unary(minus, Literal(123))
+    op = Token(TokenType.STAR, "*", None, 1)
+    rhs = Grouping(Literal(45.67))
+
+    expr = Binary(lhs, op, rhs)
+    assert ast_str(expr) == "(* (- 123) (group 45.67))"
