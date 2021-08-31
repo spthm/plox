2d4446946dc91c4a9e68214aa084559185b5d716
Sam Thomson <2648269+spthm@users.noreply.github.com>
2021-08-31 18:04:35 +0100

    [6.2] Implement parser internal methods

---
+++ src/plox/parser.py
@@ -0,0 +1,106 @@
+from plox.expressions import Binary, Expr, Grouping, Literal, Unary
+from plox.tokens import Token, TokenType
+
+
+class Parser:
+    def __init__(self, tokens: list[Token]) -> None:
+        self._tokens = tokens
+        self._current = 0
+
+    def _expression(self) -> Expr:
+        return self._equality()
+
+    def _equality(self) -> Expr:
+        expr = self._comparison()
+
+        while self._match(TokenType.BANG_EQUAL, TokenType.EQUAL_EQUAL):
+            op = self._previous()
+            rhs = self._comparison()
+            expr = Binary(expr, op, rhs)
+
+        return expr
+
+    def _comparison(self) -> Expr:
+        expr = self._term()
+
+        while self._match(
+            TokenType.GREATER,
+            TokenType.GREATER_EQUAL,
+            TokenType.LESS,
+            TokenType.LESS_EQUAL,
+        ):
+            op = self._previous()
+            rhs = self._term()
+            expr = Binary(expr, op, rhs)
+
+        return expr
+
+    def _term(self) -> Expr:
+        expr = self._factor()
+
+        while self._match(TokenType.MINUS, TokenType.PLUS):
+            op = self._previous()
+            rhs = self._factor()
+            expr = Binary(expr, op, rhs)
+
+        return expr
+
+    def _factor(self) -> Expr:
+        expr = self._unary()
+
+        while self._match(TokenType.SLASH, TokenType.STAR):
+            op = self._previous()
+            rhs = self._unary()
+            expr = Binary(expr, op, rhs)
+
+        return expr
+
+    def _unary(self) -> Expr:
+        if self._match(TokenType.BANG, TokenType.MINUS):
+            op = self._previous()
+            rhs = self._unary()
+            return Unary(op, rhs)
+
+        return self._primary()
+
+    def _primary(self) -> Expr:
+        if self._match(TokenType.TRUE):
+            return Literal(True)
+        if self._match(TokenType.FALSE):
+            return Literal(False)
+        if self._match(TokenType.NIL):
+            return Literal(None)
+
+        if self._match(TokenType.NUMBER, TokenType.STRING):
+            return Literal(self._previous().literal)
+
+        if self._match(TokenType.LEFT_PAREN):
+            expr = self._expression()
+            self._consume(TokenType.RIGHT_PAREN, "Expect ')' after expression.")
+            return Grouping(expr)
+
+    def _advance(self) -> Token:
+        if not self._at_end():
+            self._current += 1
+        return self._previous()
+
+    def _at_end(self) -> bool:
+        return self._peek().kind == TokenType.EOF
+
+    def _check(self, kind: TokenType) -> bool:
+        if self._at_end():
+            return False
+        return self._peek().kind == kind
+
+    def _match(self, *args: TokenType) -> bool:
+        if any(self._check(kind) for kind in args):
+            self._advance()
+            return True
+        return False
+
+    def _peek(self) -> Token:
+        return self._tokens[self._current]
+
+    def _previous(self) -> Token:
+        assert self._current > 0
+        return self._tokens[self._current - 1]
