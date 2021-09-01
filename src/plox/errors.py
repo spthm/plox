d5d54e4a95f759ae4ba1e8372b96c1c6b92a50a9
Sam Thomson <2648269+spthm@users.noreply.github.com>
2021-09-01 21:22:01 +0100

    Don't need to call base Exception's init.

--- src/plox/errors.py
+++ src/plox/errors.py
@@ -5,7 +5,6 @@ from plox.tokens import Token
 
 class ParserError(Exception):
     def __init__(self, message: str, token: Token) -> None:
-        super().__init__(message, token)
         self.message = message
         self.token = token
 
