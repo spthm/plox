dafa4d329f2742c8d3aae2e8813fe74a76564714
Sam Thomson <2648269+spthm@users.noreply.github.com>
2021-09-14 20:38:49 +0100

    Add assignment and scoping tests.

--- tests/test_evaluate.py
+++ tests/test_evaluate.py
@@ -1,5 +1,8 @@
-from plox.ast import Binary, Grouping, Literal, Unary, evaluate
+import pytest
+
+from plox.ast import Assign, Binary, Grouping, Literal, Unary, evaluate
 from plox.environment import Environment
+from plox.errors import ExecutionError
 from plox.tokens import Token, TokenType
 
 
@@ -26,3 +29,30 @@ def test_evaluate():
 
     assert evaluate(expr, Environment()) == 2
 # fmt: on
+
+
+def test_assignment():
+    # https://github.com/munificent/craftinginterpreters/blob/6c2ea6f7192910053a78832f0cc34ad56b17ce7c/test/assignment/grouping.lox
+    a = Token(TokenType.IDENTIFIER, "a", None, 1)
+    literal_a = Literal("a")
+
+    # var a;
+    # a = "a"
+    env = Environment()
+    env.define(a, None)
+    expr = Assign(a, literal_a)
+
+    assert evaluate(expr, env) == literal_a.value
+
+
+def test_assignment_undefined_variable():
+    # https://github.com/munificent/craftinginterpreters/blob/6c2ea6f7192910053a78832f0cc34ad56b17ce7c/test/assignment/undefined.lox
+    a = Token(TokenType.IDENTIFIER, "a", None, 1)
+    literal_a = Literal("a")
+
+    # a = "a";
+    env = Environment()
+    expr = Assign(a, literal_a)
+
+    with pytest.raises(ExecutionError, match="Undefined variable 'a'"):
+        assert evaluate(expr, env) == literal_a.value
--- tests/test_parser.py
+++ tests/test_parser.py
@@ -1,4 +1,8 @@
-from plox.ast import Binary, Grouping, Literal, Unary
+import pytest
+
+from plox.ast.expressions import Assign, Binary, Grouping, Literal, Unary, Variable
+from plox.ast.statements import Block, Expression, Var
+from plox.errors import ParserError
 from plox.parser import Parser
 from plox.tokens import Token, TokenType
 
@@ -230,3 +234,130 @@ def test_parse_precedence_equalequal_gte():
     assert statements[0].expression == Binary(
         Literal(False), equal, Binary(Literal(2.0), gte, Literal(1.0))
     )
+
+
+def test_number_leading_dot(capsys):
+    # https://github.com/munificent/craftinginterpreters/blob/6c2ea6f7192910053a78832f0cc34ad56b17ce7c/test/number/leading_dot.lox
+    dot = Token(TokenType.DOT, ".", None, 1)
+    number = Token(TokenType.NUMBER, "123", 123.0, 1)
+    semicolon = Token(TokenType.SEMICOLON, ";", None, 1)
+    end = Token(TokenType.EOF, "", None, 1)
+
+    # .123;
+    tokens = [dot, number, semicolon, end]
+
+    with pytest.raises(ParserError, match="Expect expression"):
+        Parser(tokens).parse()
+
+    _, err = capsys.readouterr()
+    assert "Error at '.': Expect expression." in err
+
+
+def test_associativity():
+    # https://github.com/munificent/craftinginterpreters/blob/6c2ea6f7192910053a78832f0cc34ad56b17ce7c/test/assignment/associativity.lox
+    equals = Token(TokenType.EQUAL, "=", None, 1)
+    a = Token(TokenType.IDENTIFIER, "a", None, 1)
+    b = Token(TokenType.IDENTIFIER, "b", None, 1)
+    c = Token(TokenType.IDENTIFIER, "c", None, 1)
+    semicolon = Token(TokenType.SEMICOLON, ";", None, 1)
+    end = Token(TokenType.EOF, "", None, 1)
+
+    # a = b = c;
+    tokens = [
+        a,
+        equals,
+        b,
+        equals,
+        c,
+        semicolon,
+        end,
+    ]
+
+    statements = Parser(tokens).parse()
+    assert len(statements) == 1
+
+    assert statements[0] == Expression(Assign(a, Assign(b, Variable(c))))
+
+
+def test_assign_to_group_fails(capsys):
+    # https://github.com/munificent/craftinginterpreters/blob/6c2ea6f7192910053a78832f0cc34ad56b17ce7c/test/assignment/grouping.lox
+    equals = Token(TokenType.EQUAL, "=", None, 1)
+    a = Token(TokenType.IDENTIFIER, "a", None, 1)
+    string_a = Token(TokenType.STRING, '"a"', "a", 1)
+    lbracket = Token(TokenType.LEFT_PAREN, "(", None, 1)
+    rbracket = Token(TokenType.RIGHT_PAREN, ")", None, 1)
+    semicolon = Token(TokenType.SEMICOLON, ";", None, 1)
+    end = Token(TokenType.EOF, "", None, 1)
+
+    # (a) = "a";
+    tokens = [
+        lbracket,
+        a,
+        rbracket,
+        equals,
+        string_a,
+        semicolon,
+        end,
+    ]
+
+    with pytest.raises(ParserError, match="Invalid assignment target"):
+        Parser(tokens).parse()
+
+    _, err = capsys.readouterr()
+    assert "Error at '=': Invalid assignment target." in err
+
+
+def test_block():
+    # https://github.com/munificent/craftinginterpreters/blob/6c2ea6f7192910053a78832f0cc34ad56b17ce7c/test/block/scope.lox
+    equals = Token(TokenType.EQUAL, "=", None, 1)
+    a = Token(TokenType.IDENTIFIER, "a", None, 1)
+    inner = Token(TokenType.STRING, '"inner"', "inner", 1)
+    outer = Token(TokenType.STRING, '"outer"', "outer", 1)
+    lbrace = Token(TokenType.LEFT_BRACE, "{", None, 1)
+    rbrace = Token(TokenType.RIGHT_BRACE, "}", None, 1)
+    semicolon = Token(TokenType.SEMICOLON, ";", None, 1)
+    var = Token(TokenType.VAR, "var", None, 1)
+    end = Token(TokenType.EOF, "", None, 1)
+
+    # a = "inner";
+    # { var a = "outer"; }
+    tokens = [
+        a,
+        equals,
+        outer,
+        semicolon,
+        lbrace,
+        var,
+        a,
+        equals,
+        inner,
+        semicolon,
+        rbrace,
+        end,
+    ]
+
+    statements = Parser(tokens).parse()
+    assert len(statements) == 2
+
+    assert statements[0] == Expression(Assign(a, Literal("outer")))
+    assert statements[1] == Block([Var(a, Literal("inner"))])
+
+
+def test_print_no_expression(capsys):
+    # https://github.com/munificent/craftinginterpreters/blob/6c2ea6f7192910053a78832f0cc34ad56b17ce7c/test/print/missing_argument.lox
+    print_ = Token(TokenType.PRINT, "print", None, 1)
+    semicolon = Token(TokenType.SEMICOLON, ";", None, 1)
+    end = Token(TokenType.EOF, "", None, 1)
+
+    # print;
+    tokens = [
+        print_,
+        semicolon,
+        end,
+    ]
+
+    with pytest.raises(ParserError, match="Expect expression"):
+        Parser(tokens).parse()
+
+    _, err = capsys.readouterr()
+    assert "Error at ';': Expect expression." in err
